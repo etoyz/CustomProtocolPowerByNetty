d8680545e53c57a90013735431b09b0c052e1f9b
etoyz <2905226519@qq.com>
2022-03-16 20:20:43 +0800

    打通前后端

--- NettyDemoWithWeb/src/main/java/com/example/nettydemowithweb/netty/Server.java
+++ NettyDemoWithWeb/src/main/java/com/example/nettydemowithweb/netty/Server.java
@@ -1,16 +1,19 @@
 package com.example.nettydemowithweb.netty;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Map;
+
+@RestController
 @RequestMapping("/")
 public class Server {
 
-    @ResponseBody
-    @RequestMapping("/")
-    public String test() {
-        return "Ready!";
+    @RequestMapping("uploadRawData")
+    public String uploadRawData(@RequestParam Map<String, String> map) {
+        System.out.println(map);
+
+        return "ok";
     }
 }
