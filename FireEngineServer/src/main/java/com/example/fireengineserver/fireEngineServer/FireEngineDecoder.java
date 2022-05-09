9d3d5a0017ca1b4824300779fd44a9491a720f10
etoyz <2905226519@qq.com>
2022-05-09 13:15:00 +0800

    add doc

--- FireEngineServer/src/main/java/com/example/fireengineserver/fireEngineServer/FireEngineDecoder.java
+++ FireEngineServer/src/main/java/com/example/fireengineserver/fireEngineServer/FireEngineDecoder.java
@@ -20,19 +20,19 @@ public class FireEngineDecoder extends SimpleChannelInboundHandler<DatagramPacke
         System.out.println("\n\n收到UDP数据报：\t" + msg.toString());
         byte[] bytes = new byte[msg.content().readableBytes()];
         msg.content().readBytes(bytes);
-        String binaryCodeString = new String(bytes, StandardCharsets.UTF_8);
+        String binaryCodeString = new String(bytes, StandardCharsets.UTF_8); // 表示二进制的字符串
         System.out.println("提取到数据部分：\t" + binaryCodeString);
 
         // 将表示二进制的字符串转为原始二进制数字
-        BigInteger integer = new BigInteger(binaryCodeString, 2);
-        String hexStr = integer.toString(16).toUpperCase(Locale.ROOT);
+        BigInteger binaryCode = new BigInteger(binaryCodeString, 2);
+        String hexStr = binaryCode.toString(16).toUpperCase(Locale.ROOT);
         System.out.println("十六进制表示：\t\t" + hexStr + "H");
 
         // 将接收到的消防主机数据存储下来(待处理) // TODO
         FireEngineServer.receivedStatus.add(new String[]{
-                msg.sender().getHostString() + ":" + msg.sender().getPort(),
-                hexStr + "H",
-                new SimpleDateFormat("MM/dd HH:mm:ss").format(new Date())
+                msg.sender().getHostString() + ":" + msg.sender().getPort(), // 请求者的IP和端口
+                hexStr + "H", // 接收到的数据的16进制表示
+                new SimpleDateFormat("MM/dd HH:mm:ss").format(new Date()) // 接收到数据的时间
         });
 
 
