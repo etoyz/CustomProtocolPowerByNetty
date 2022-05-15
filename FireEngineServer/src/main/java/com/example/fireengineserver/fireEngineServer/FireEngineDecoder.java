4351d068cfff354e96c1d8f512365591cc6752b5
wrf <vbty4540@gmail.com>
2022-05-15 22:10:11 +0800

    wrf first contribution

--- FireEngineServer/src/main/java/com/example/fireengineserver/fireEngineServer/FireEngineDecoder.java
+++ FireEngineServer/src/main/java/com/example/fireengineserver/fireEngineServer/FireEngineDecoder.java
@@ -28,8 +28,28 @@ public class FireEngineDecoder extends SimpleChannelInboundHandler<DatagramPacke
         String hexStr = binaryCode.toString(16).toUpperCase(Locale.ROOT);
         System.out.println("十六进制表示：\t\t" + hexStr + "H");
 
+        //将接收到的消防主机数据存储下来(待处理)
+        String DataMessage = null;
+        if (hexStr.substring(10, 12).equals("60")) {
+            if (hexStr.substring(12, 14).equals("00")) {
+                String strnum1 = hexStr.substring(18, 22);
+                int intnum1 = Integer.parseInt(strnum1,16);
+                String strnum2 = hexStr.substring(22, 26);
+                int intnum2 = Integer.parseInt(strnum2,16);
+                String strnum3 = hexStr.substring(26, 30);
+                int intnum3 = Integer.parseInt(strnum3,16);
+                DataMessage = "系统信息：总报警个数" + intnum1 + "  " + "总故障个数" + intnum2 + "  " + "总探测器个数" + intnum3;
+            }
+            else {
+                DataMessage = "读取失败！";
+            }
+        } else {
+            DataMessage="暂不支持";
+        }
+
+
         // 将接收到的消防主机数据存储下来(待处理)
-        String convertedMessage = "待处理";
+        String convertedMessage = DataMessage;
         // TODO
         FireEngineServer.receivedStatus.add(new String[]{
                 msg.sender().getHostString() + ":" + msg.sender().getPort(), // 请求者的IP和端口
