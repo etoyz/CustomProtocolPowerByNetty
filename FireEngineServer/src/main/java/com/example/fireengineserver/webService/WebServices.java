0baae3d4a2912adf75ee145bac9eddc2ff492a6a
etoyz <2905226519@qq.com>
2022-05-18 10:33:43 +0800

    :sparkles: 在页面上查看运行日志

--- FireEngineServer/src/main/java/com/example/fireengineserver/webService/WebServices.java
+++ FireEngineServer/src/main/java/com/example/fireengineserver/webService/WebServices.java
@@ -5,9 +5,12 @@ import com.example.fireengineserver.fireEngineServer.RequestMsg;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 @RestController
 @RequestMapping("/")
@@ -27,6 +30,25 @@ public class WebServices {
     public String clearData() {
         FireEngineServer.receivedStatus.clear();
         return "ok";
+    }
+
+    @RequestMapping("viewLog")
+    public String viewLog() throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        Scanner scanner = new Scanner(Paths.get("server.log"));
+        stringBuilder.append("<h1>服务器全部运行日志</h1>");
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.endsWith("info") || line.endsWith("warning"))
+                stringBuilder
+                        .append("<hr style='height:1px;background-color:red'><strong>")
+                        .append(line)
+                        .append("</strong>");
+            else
+                stringBuilder.append("<div>").append(line).append("</div>");
+        }
+        scanner.close();
+        return stringBuilder.toString();
     }
 }
 
